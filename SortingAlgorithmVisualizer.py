47b0a7072485d69bedf4e122135c0e8f365e1f54
zainaraza43 <46791180+zainaraza43@users.noreply.github.com>
2020-02-20 01:26:58 -0500

    added some dividing updater

--- SortingAlgorithmVisualizer.py
+++ SortingAlgorithmVisualizer.py
@@ -37,6 +37,13 @@ def BubbleSort(list):
 		for j in range(0,len(list)-i-1):
 			if list[j].value > list[j+1].value:
 				list[j], list[j+1] = list[j+1], list[j]
+		if (i % 5 == 0) or (i == (len(list) - 1)):
+			for bar in bars:
+				pygame.draw.rect(screen, bar.colour, pygame.Rect(bars.index(bar) + 1, 1000 - bar.value, 1, bar.value))
+				pygame.display.flip()
+		if i != (len(list) - 1):
+			print(i, len(list))
+			screen.fill(0)
 
 pygame.init()
  
@@ -53,27 +60,23 @@ done = False
 clock = pygame.time.Clock()
  
 # -------- Main Program Loop -----------
+screen.fill(BLACK)
 while not done:
-    # --- Main event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+	# --- Main event loop
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			done = True
 
-    screen.fill(BLACK)
+	if currentAlgo in algos:
+		if currentAlgo == "Bubble Sort":
+			BubbleSort(bars)
+		currentAlgo = "None"
 
-    # Displays the bars
-    for bar in bars: 
-    	pygame.draw.rect(screen,bar.colour,pygame.Rect(bars.index(bar)+1,1000-bar.value,1,bar.value))
 
-    if currentAlgo in algos:
-    	if currentAlgo == "Bubble Sort":
-    		BubbleSort(bars)
-    	currentAlgo = "None"
+	pygame.display.flip()
 
-    pygame.display.flip()
- 
-    # Limit to 60 frames per second
-    clock.tick(60)
- 
+	# Limit to 60 frames per second
+	# clock.tick(60)
+
 # Close the window and quit.
 pygame.quit()
